9519c599018e8618f8745c01a14bf1b7468b1499
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-18 23:55:28 +0600

    finding elements in index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// finding the elements
+const container = document.querySelector('.container');
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('#inputTodo');
+const addTodoButton = document.querySelector('#addTodoButton');
