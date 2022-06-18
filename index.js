74546d301c8f56de24522b3c1cad38acc31937bb
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-18 23:59:14 +0600

    adding listener to the form

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const container = document.querySelector('.container');
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('#inputTodo');
 const addTodoButton = document.querySelector('#addTodoButton');
+
+
+// addTodo
+const addTodo = (event) => {
+    event.preventDefault();
+    console.log(todoInput.value);
+}
+
+// adding listeners 
+todoForm.addEventListener('submit', addTodo)
