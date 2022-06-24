191d89a0765f4e629df7b0df76a3ebee49ca08c9
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-24 22:23:11 +0600

    create element

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const container = document.querySelector('.container');
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('#inputTodo');
 const addTodoButton = document.querySelector('#addTodoButton');
+const todoLists = document.querySelector('#lists');
 
+// createTodo
+const createTodo = (todoId, todoValue) =>{
+    const todoElement = document.createElement('li');
+    todoElement.id = todoId;
+    todoElement.classList.add('li-style');
+    todoElement.innerHTML = `
+    <span> ${todoValue} </span>
+    <span> <button class='btn'><i class="fas fa-trash"></i></button> </span>
+    `;
+    todoLists.appendChild(todoElement);
+}
 
 // addTodo
 const addTodo = (event) => {
     event.preventDefault();
-    console.log(todoInput.value);
+    const todoValue = todoInput.value;
+
+
+    // unique id
+    const todoId = Date.now().toString();
+    createTodo(todoId, todoValue);
+
 }
 
 // adding listeners 
